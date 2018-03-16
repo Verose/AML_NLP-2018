6d23d9f5b18a06a36f6d8d13205f473151719cf9
Dvir <dvirginz@gmail.com>
2018-03-16 09:25:58 +0200

    NLP1 starter code and word

---
+++ NLP1/NLP1/NLP1/q1b_softmax.py
@@ -0,0 +1,62 @@
+import numpy as np
+
+
+def softmax(x):
+    
+    orig_shape = x.shape
+
+    if len(x.shape) > 1:
+        x = sub_max_softmax(x)
+        x = np.exp(x)
+        soft_norm = np.sum(x,1)
+        x = x / soft_norm
+
+    else:
+        x = sub_max_softmax(x)
+        x = np.exp(x)
+        soft_norm = np.sum(x,0)
+        x = x / soft_norm
+
+    assert x.shape == orig_shape
+    return x
+
+
+def test_softmax_basic():
+    """
+    Some simple tests to get you started.
+    Warning: these are not exhaustive.
+    """
+    print "Running basic tests..."
+    test1 = softmax(np.array([1,2]))
+    print test1
+    ans1 = np.array([0.26894142,  0.73105858])
+    assert np.allclose(test1, ans1, rtol=1e-05, atol=1e-06)
+
+    test2 = softmax(np.array([[1001,1002],[3,4]]))
+    print test2
+    ans2 = np.array([
+        [0.26894142, 0.73105858],
+        [0.26894142, 0.73105858]])
+    assert np.allclose(test2, ans2, rtol=1e-05, atol=1e-06)
+
+    test3 = softmax(np.array([[-1001,-1002]]))
+    print test3
+    ans3 = np.array([0.73105858, 0.26894142])
+    assert np.allclose(test3, ans3, rtol=1e-05, atol=1e-06)
+
+    print "You should be able to verify these results by hand!\n"
+
+
+def test_softmax():
+    pass
+
+def sub_max_softmax(x_arr):
+  max_vec = (np.amax(x_arr, axis=1))[:,None] if len(x_arr.shape) > 1 else (np.amax(x_arr, axis=0))
+  #max_vec = (np.amax(x_arr, axis=axis))[:,None]
+  x_sub = x_arr - max_vec
+  return x_sub
+
+
+if __name__ == "__main__":
+    test_softmax_basic()
+    test_softmax()
---
+++ NLP1/NLP1/NLP1/q1d_sigmoid.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+import numpy as np
+
+
+def sigmoid(x):
+    """
+    Compute the sigmoid function for the input here.
+
+    Arguments:
+    x -- A scalar or numpy array.
+
+    Return:
+    s -- sigmoid(x)
+    """
+    s = 1 + np.exp(-x)
+    s = 1 / s
+
+    return s
+
+
+def sigmoid_grad(s):
+    sub_s = 1 - s
+    ds = np.multiply([s,sub_s])
+    return ds
+
+def test_sigmoid_basic():
+    print "Running basic tests..."
+    x = np.array([[1, 2], [-1, -2]])
+    f = sigmoid(x)
+    g = sigmoid_grad(f)
+    print f
+    f_ans = np.array([
+        [0.73105858, 0.88079708],
+        [0.26894142, 0.11920292]])
+    assert np.allclose(f, f_ans, rtol=1e-05, atol=1e-06)
+    print g
+    g_ans = np.array([
+        [0.19661193, 0.10499359],
+        [0.19661193, 0.10499359]])
+    assert np.allclose(g, g_ans, rtol=1e-05, atol=1e-06)
+    print "You should verify these results by hand!\n"
+
+def test_sigmoid():
+    """
+    Use this space to test your sigmoid implementation by running:
+        python q2_sigmoid.py
+    This function will not be called by the autograder, nor will
+    your tests be graded.
+    """
+    
+if __name__ == "__main__":
+    test_sigmoid_basic()
+    test_sigmoid()
