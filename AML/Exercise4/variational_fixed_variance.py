db2a8f4cddb1dd0151cf9bc772090813250d792d
dvirg <dvirginz@gmail.com>
2018-05-26 19:15:16 +0300

    fixed non variance

--- AML/Exercise4/variational_fixed_variance.py
+++ AML/Exercise4/variational_fixed_variance.py
@@ -29,18 +29,20 @@ epsilon_std = 1.0
 x = Input(shape=(original_dim,))
 h = Dense(intermediate_dim, activation='relu')(x)
 z_mean = Dense(latent_dim)(h)
-z_log_var = Dense(latent_dim)(h)
+
+z_log_var = Dense(latent_dim, trainable=False,kernel_initializer='zeros',bias_initializer='zeros')(h)
+
 
 
 def sampling(args):
     z_mean, z_log_var = args
     epsilon = K.random_normal(shape=(K.shape(z_mean)[0], latent_dim), mean=0.,
                               stddev=epsilon_std)
-    return z_mean
+    return z_mean + K.exp(z_log_var / 2) * epsilon
 
 z = Lambda(sampling, output_shape=(latent_dim,))([z_mean, z_log_var])
 
-encoder = Model(x, [z_mean, z_log_var, z])
+encoder = Model(x, z_mean)
 encoder.summary()
 
 # we instantiate these layers separately so as to reuse them later
@@ -59,7 +61,7 @@ vae = Model(x, x_decoded_mean)
 
 # Compute VAE loss
 xent_loss = original_dim * metrics.binary_crossentropy(x, x_decoded_mean)
-kl_loss = - 0.5 * K.sum(- K.square(z_mean), axis=-1)
+kl_loss = - 0.5 * K.sum(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis=-1)
 vae_loss = K.mean(xent_loss + kl_loss)
 
 vae.add_loss(vae_loss)
